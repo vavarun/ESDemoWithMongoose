78a99b4d793893233695824efb3fbf0fdc2abbb0
nodkz <pavel.chertorogov@gmail.com>
2017-10-11 12:27:16 +0600

    fix: searchConnection (solve problem https://github.com/nodkz/graphql-compose-elasticsearch/issues/22#issuecomment-335446113)

--- server/schema/Query.js
+++ server/schema/Query.js
@@ -6,17 +6,20 @@ const { CustomerEsTC, CustomerTC } = require('./Customer');
 const QueryTC = GQC.rootQuery();
 
 QueryTC.addFields({
-  agentEsConnection: AgentEsTC.getResolver('search'),
+  agentEsSearch: AgentEsTC.getResolver('search'),
+  agentEsConnection: AgentEsTC.getResolver('searchConnection'),
   agentMongoConnection: AgentTC.getResolver('connection'),
   agentMany: AgentTC.getResolver('findMany'),
   agent: AgentTC.getResolver('findOne'),
   agentById: AgentTC.getResolver('findById'),
-  appointmentEsConnection: AppointmentEsTC.getResolver('search'),
+  appointmentEsSearch: AppointmentEsTC.getResolver('search'),
+  appointmentEsConnection: AppointmentEsTC.getResolver('searchConnection'),
   appointmentMongoConnection: AppointmentTC.getResolver('connection'),
   appointmentMany: AppointmentTC.getResolver('findMany'),
   appointment: AppointmentTC.getResolver('findOne'),
   appointmentById: AppointmentTC.getResolver('findById'),
-  customerEsConnection: CustomerEsTC.getResolver('search'),
+  customerEsSearch: CustomerEsTC.getResolver('search'),
+  customerEsConnection: CustomerEsTC.getResolver('searchConnection'),
   customerMongoConnection: CustomerTC.getResolver('connection'),
   customerMany: CustomerTC.getResolver('findMany'),
   customer: CustomerTC.getResolver('findOne'),
